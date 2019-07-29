042b4ebf925e8cd9daf25166ebc6e3cda82f7db1
Roman Gruba <roman.gruba@gmail.com>
2019-07-29 11:05:27 +0300

    finished. before collection

---
+++ index.js
@@ -0,0 +1,76 @@
+import galleryData from "./gallery-items.js";
+
+const ul = document.querySelector(".gallery");
+
+const fragment = document.createDocumentFragment();
+
+function newItem(obj) {
+  const li = document.createElement("li");
+  li.classList.add("gallery__item");
+
+  const a = document.createElement("a");
+  a.classList.add("gallery__link");
+  a.setAttribute("href", obj.original);
+
+  const img = document.createElement("img");
+  img.classList.add("gallery__image");
+  img.setAttribute("src", obj.preview);
+  img.setAttribute("data-source", obj.original);
+  img.setAttribute("alt", obj.description);
+
+  const span = document.createElement("span");
+  span.classList.add("gallery__icon");
+
+  const i = document.createElement("i");
+  i.classList.add("material-icons");
+  i.textContent = "zoom_out_map";
+
+  span.append(i);
+  a.append(img, span);
+  li.append(a);
+  fragment.append(li);
+  return fragment;
+}
+
+galleryData.forEach(el => {
+  newItem(el);
+});
+
+ul.append(fragment);
+
+ul.addEventListener("click", openModal);
+
+function openModal(e) {
+  e.preventDefault();
+  if (e.target !== e.currentTarget) {
+    document.querySelector(".lightbox").classList.add("is-open");
+    const currentImage = document.querySelector(".lightbox___image");
+    currentImage.setAttribute("src", e.target.getAttribute(["data-source"]));
+    document.querySelector(".is-open").addEventListener("click", e => {
+      if (e.target !== document.querySelector(".lightbox___image")) {
+        closeModal();
+      }
+    });
+    window.addEventListener("keydown", handleKeyPress);
+  }
+}
+
+function closeModal() {
+  document.querySelector(".lightbox").classList.remove("is-open");
+  const currentImage = document.querySelector(".lightbox___image");
+  currentImage.setAttribute("src", "");
+  window.removeEventListener("keydown", handleKeyPress);
+  closeButton.removeEventListener("click", closeModal);
+}
+
+const closeButton = document.querySelector(
+  'button[data-action="close-lightbox"]'
+);
+
+closeButton.addEventListener("click", closeModal);
+
+function handleKeyPress(e) {
+  if (e.code !== "Escape") {
+    return;
+  } else closeModal();
+}
